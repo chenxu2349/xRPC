706cfab847e433cfa6c791bc039b29e5a574c68f
chenxu <chenxu_2349@qq.com>
2023-11-21 21:23:55 +0800

    通过动态代理调用方法

--- Consumer/src/main/java/com/chenxu/Consumer.java
+++ Consumer/src/main/java/com/chenxu/Consumer.java
@@ -2,20 +2,24 @@ package com.chenxu;
 
 import com.chenxu.common.Invocation;
 import com.chenxu.protocol.HttpClient;
+import com.chenxu.proxy.ProxyFactory;
 
 public class Consumer {
 
     public static void main(String[] args) {
-//        HelloService helloService = ?;
-//        String result = helloService.sayHello("tom");
-//        System.out.println(result);
 
-        // Invocation对象通过请求的方式发送出去
-        Invocation invocation = new Invocation(HelloService.class.getName(), "sayHello",
-                new Class[]{String.class}, new Object[]{"chenxu"});
-
-        HttpClient httpClient = new HttpClient();
-        String result = httpClient.send("localhost", 8080, invocation);
+        // 调用方法一：动态代理
+        HelloService helloService = ProxyFactory.getProxy(HelloService.class);
+        String result = helloService.sayHello("tom"); // 调用这里的方法会进到proxyinstance里的invoke
         System.out.println(result);
+
+        // 调用方法二
+        // Invocation对象通过请求的方式发送出去
+//        Invocation invocation = new Invocation(HelloService.class.getName(), "sayHello",
+//                new Class[]{String.class}, new Object[]{"chenxu"});
+//
+//        HttpClient httpClient = new HttpClient();
+//        String result = httpClient.send("localhost", 8080, invocation);
+//        System.out.println(result);
     }
 }
